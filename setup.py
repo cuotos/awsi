cbb0579f96ef274fe47b7c38ca7edc2413f871f0
Dan Potepa <dan@danpotepa.co.uk>
2017-01-10 17:38:23 +0000

    added more to setup file ready to go to pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+def readme():
+  with open("README.md") as f:
+    return f.read()
+
 setup(name='awsi',
       version='1.1',
-      description='AWSI',
+      description='Amazon Web Services information and connection tool',
+      long_description=readme(),
       url='http://gitlab.com/cuotos/awsi',
       author='Dan Potepa',
       author_email='dan@danpotepa.co.uk',
